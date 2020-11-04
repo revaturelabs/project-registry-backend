635b21cb3e0dfe63e704807ca5e9404c9699a752
William Ona <william.ona@revature.com>
2020-11-04 17:08:49 -0600

    added mock mvc test for createProject

--- src/test/java/com/revature/registry/controller/ProjectControllerTest.java
+++ src/test/java/com/revature/registry/controller/ProjectControllerTest.java
@@ -2,8 +2,10 @@ package com.revature.registry.controller;
 
 import static org.hamcrest.CoreMatchers.isA;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
@@ -13,16 +15,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.registry.ProjectRegistryBackendApplication;
 import com.revature.registry.model.Project;
 import com.revature.registry.service.ProjectService;
@@ -96,7 +102,25 @@ class ProjectControllerTest {
 			.andExpect(MockMvcResultMatchers.jsonPath("$.description").value("a project simply just for testing"));
 	}
 	
+	@Test
+	public void should_create_project_when_valid_input() throws Exception {
+		//mock the return of createProject from ProjectService
+		Project project = new Project();
+		project.setId(12);
+		project.setName("testProject");
+		project.setDescription("a project simply just for testing");
+		
+		when(projectService.createProject(any(Project.class)))
+			.thenReturn(new ResponseEntity<Project>(project,HttpStatus.CREATED));
+		
+		mockMvc.perform(post("/api/project")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(project)))
+			.andExpect(status().isCreated())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("testProject"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(12))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.description").value("a project simply just for testing"));
+	}
 	
-
-
+	
 }
