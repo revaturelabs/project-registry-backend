927292d4ef18284382903b6f5fd60d4b92df149c
William Ona <william.ona@revature.com>
2020-11-04 09:56:27 -0600

    developed endpoints for: getAll, getById, update, delete, create

--- src/main/java/com/revature/registry/controller/ProjectController.java
+++ src/main/java/com/revature/registry/controller/ProjectController.java
@@ -3,8 +3,13 @@ package com.revature.registry.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +24,27 @@ public class ProjectController {
 	private ProjectService projectService;
 	
 	@GetMapping("")
-	public List<Project> getAllProjects(){
+	public ResponseEntity<List<Project>> getAllProjects(){
 		return projectService.getAllProjects();
 	}
 	
+	@GetMapping("/id/{id}")
+	public ResponseEntity<Project> getProjectById(@PathVariable("id") int id){
+		return projectService.getProjectById(id);
+	}
+
 	@PostMapping("")
-	public Project postProject(@RequestBody Project project) {
-		return projectService.saveProject(project);
+	public ResponseEntity<Project> createProject(@RequestBody Project project) {
+		return projectService.createProject(project);
+	}
+	
+	@PutMapping("id/{id}")
+	public ResponseEntity<Project> updateProject(@PathVariable("id") int id, @RequestBody Project project) {
+		return projectService.updateProject(id, project);
+	}
+	
+	@DeleteMapping("id/{id}")
+	public ResponseEntity<Project> deleteUser(@PathVariable("id") int id) {
+		return projectService.deleteProjectById(id);
 	}
 }
--- src/main/java/com/revature/registry/service/ProjectService.java
+++ src/main/java/com/revature/registry/service/ProjectService.java
@@ -1,23 +1,67 @@
 package com.revature.registry.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.revature.registry.model.Project;
 import com.revature.registry.repository.ProjectRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class ProjectService {
 	@Autowired
 	private ProjectRepository projectRepository;
 
-	public List<Project> getAllProjects() {
-		return projectRepository.findAll();
+	public ResponseEntity<List<Project>> getAllProjects() {
+		log.debug("Fetching all products: {}", projectRepository.findAll());
+		return new ResponseEntity<List<Project>>(projectRepository.findAll(), HttpStatus.OK);
+	}
+	
+	public ResponseEntity<Project> getProjectById(int id) {
+		Optional<Project> project = projectRepository.findById(id);
+		if(project.isPresent()) {
+			log.debug("Fetching Project with id of {}: {}", project.get());
+			return new ResponseEntity<Project>( project.get(), HttpStatus.OK);
+		}
+		log.error("Unable to GET. User with id {} not found.", id);
+		return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
 	}
 
-	public Project saveProject(Project project) {
-		return projectRepository.save(project);
+	public ResponseEntity<Project> createProject(Project project) {
+		Project savedProject = projectRepository.save(project);
+		log.debug("Project created with the following properties: {}", savedProject);
+		return new ResponseEntity<Project>(savedProject, HttpStatus.CREATED);
 	}
+	
+	public ResponseEntity<Project> updateProject(int id, Project newProject) {
+		Optional<Project> project = projectRepository.findById(id);
+		if(project.isPresent()) {
+			newProject.setId(id);
+			projectRepository.save(newProject);
+			log.debug("Project with id: {}. Updated with the following proprerties: {}", id, newProject);
+			return new ResponseEntity<Project>(project.get(), HttpStatus.OK);
+		}
+
+		log.error("Unable to update. User with id {} not found.", id);
+		return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
+	}
+	
+	public ResponseEntity<Project> deleteProjectById(int id) {
+		Optional<Project> project = projectRepository.findById(id);
+		if(project.isPresent()) {
+			projectRepository.deleteById(id);
+			log.debug("Project Deleted with id: {}", id);
+			return new ResponseEntity<Project>(HttpStatus.NO_CONTENT);
+		}
+		log.error("Unable to DELETE. User with id {} not found.", id);
+		return  new ResponseEntity<Project>(HttpStatus.BAD_REQUEST);
+	}
+
 }
