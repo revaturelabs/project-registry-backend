86aa2fcbd29d33cf71d6fb0bee9c39f427ca8eee
christian-downs <christian.downs.15@gmail.com>
2021-04-06 11:40:55 -0500

    added the crossorigins

--- src/main/java/com/revature/registry/controller/TagController.java
+++ src/main/java/com/revature/registry/controller/TagController.java
@@ -8,6 +8,7 @@ import com.revature.registry.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 
+
+
 @RestController
 @RequestMapping(value = "/api/Tag", produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin(origins = "http://localhost:4200")
 public class TagController {
     @Autowired
     private TagService tagService;
